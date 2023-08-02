91e7c206c6e8bd7f07778b75cb2af3fc2de26805
iqbalhossain4 <iqbal-hossain@mail.com>
2023-08-02 20:16:40 +0600

    implement update status

--- src/Components/TaskList/TaskList.jsx
+++ src/Components/TaskList/TaskList.jsx
@@ -1,9 +1,31 @@
 import React from "react";
 import useTask from "../../Hooks/useTask";
+import Swal from "sweetalert2";
 
 const TaskList = () => {
   const [storeTask] = useTask();
   //   handle Status and Update Status
+  const handleStatus = (id) => {
+    fetch(`http://localhost:5000/taskStatus/${id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ status: "Completed" }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "successfully Updated Your Task",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
   return (
     <div>
       <table className="table table-dark table-striped">
@@ -22,7 +44,18 @@ const TaskList = () => {
               <th scope="row">{index + 1}</th>
               <td>{task.title}</td>
               <td>{task.description.slice(0, 25)}</td>
-              <td onClick={() => handleStatus(task._id)}>{task.status}</td>
+              <td>
+                <button
+                  onClick={() => handleStatus(task._id)}
+                  className={`makePointer rounded  ${
+                    task.status == "incomplete"
+                      ? "text-bg-danger "
+                      : "text-bg-success"
+                  }`}
+                >
+                  {task.status}
+                </button>
+              </td>
               <td>{task.status}</td>
             </tr>
           </tbody>
